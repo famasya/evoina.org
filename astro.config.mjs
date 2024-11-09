2339522ea9b8fc72cb16d5bdffb4dd283d976244
Abid Famasya <abid.famasya@gmail.com>
2024-11-09 15:10:31 +0700

    feat: final design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import tailwind from '@astrojs/tailwind';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react(), icon()],
 });
